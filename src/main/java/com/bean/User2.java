f108b6f9826b7443932d26a33b6797218cdb247e
Administrator <645874679@qq.com>
2020-04-05 13:09:08 +0800

    新增xml

---
+++ src/main/java/com/bean/User2.java
@@ -0,0 +1,33 @@
+
+package com.bean;
+
+public class User2 {
+
+	private String name;
+
+	private String password;
+
+	public User2() {
+	}
+
+	public String getName() {
+
+		return name;
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+	}
+
+	public String getPassword() {
+
+		return password;
+	}
+
+	public void setPassword(String password) {
+
+		this.password = password;
+	}
+
+}
